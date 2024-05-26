d75b8c1a9acd43696447375d057f450862145265
HamzaBenslimanePro <144680356+HamzaBenslimanePro@users.noreply.github.com>
2024-05-26 21:26:56 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,13 @@
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import os
 
 # Check the storage type from environment variables
